d661cd9238a5a16e1cd51803571c9bdfc177551a
ahmads1990 <ahmads20002000@gmail.com>
2024-10-22 15:38:08 +0300

    Dockerize app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
-import { CartsModule } from './carts/carts.module';
 import { CartItemsModule } from './cart-items/cart-items.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), ProductsModule, UsersModule, CartsModule, CartItemsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    ProductsModule,
+    UsersModule,
+    CartItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
